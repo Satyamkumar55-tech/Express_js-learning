66de6db3f296202e6c8aa108fb47a6272749f50f
Satyamkumar Singh <satyamranjeet2006@gmail.com>
2026-09-06 20:07:38 +0530

    Created another Student API project with mongoose

---
+++ Student REST API Mongoose/index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+
+
+async function startServer() {
+    try {
+
+        await mongoose.connect(process.env.MONGODB_URI, {
+            family: 4
+        });
+
+        console.log("MongoDB connected through Mongoose!");
+
+        app.listen(3000, () => {
+            console.log("Server running on port 3000");
+        });
+
+    } catch (error) {
+
+        console.error("MongoDB connection failed:", error);
+
+    }
+}
+
+startServer();
