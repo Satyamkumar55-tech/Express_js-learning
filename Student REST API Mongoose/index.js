19a9cfda46eeebd67cc969bd48042cb0367da561
Satyamkumar Singh <satyamranjeet2006@gmail.com>
2026-09-21 20:35:28 +0530

    Refactor registration logic in authController and improve error handling

--- Student REST API Mongoose/index.js
+++ Student REST API Mongoose/index.js
@@ -5,36 +5,29 @@ const mongoose = require("mongoose");
 
 const studentRoutes = require("./routes/studentRoutes");
 const authRoutes = require("./routes/authRoutes");
-app.use("/auth", authRoutes);
-
 const app = express();
 
 app.use(express.json());
 
-
 async function startServer() {
-    try {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      family: 4,
+    });
 
-        await mongoose.connect(process.env.MONGODB_URI, {
-            family: 4
-        });
+    console.log("MongoDB connected through Mongoose!");
 
-        console.log("MongoDB connected through Mongoose!");
+    // Student Routes
+    app.use("/students", studentRoutes);
 
+    app.use("/auth", authRoutes);
 
-        // Student Routes
-        app.use("/students", studentRoutes);
-
-
-        app.listen(3000, () => {
-            console.log("Server running on port 3000");
-        });
-
-    } catch (error) {
-
-        console.error("MongoDB connection failed:", error);
-
-    }
+    app.listen(3000, () => {
+      console.log("Server running on port 3000");
+    });
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+  }
 }
 
 startServer();
